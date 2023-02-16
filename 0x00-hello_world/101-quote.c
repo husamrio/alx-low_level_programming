8d55954be87befef1af319ff0061e4539e67f6af
husamrio <zulfiqrd@gmail.com>
2023-02-16 18:19:22 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -9,6 +9,6 @@ nclude <stdio.h>
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
