4883701b9b1d5ecd0fbeec62c003bdaf3aa3310e
Hussein Abdullahi <123378862+husamrio@users.noreply.github.com>
2023-02-17 20:50:59 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ int main(void)
 	if ((n % 10) == 0)
 	{
 		printf("0\n");
-	}		
+	}
 	else if ((n % 10) > 5)
 	{
 		printf("greater than 5\n");
-	}		
+	}
 	else
+	{
+		printf("less than 6 and not 0\n");
 	}
-	printf("less than 6 and not 0\n");
-	}
-	
+
 	return (0);
 }
