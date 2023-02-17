cdc04dff79f8b416868976ac1773f111194b494a
husamrio <zulfiqrd@gmail.com>
2023-02-17 20:19:17 +0300

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,7 +8,7 @@
  */
 
 int main(void)
-{		
+{	
 	int n;
 
 	srand(time(0));
@@ -21,13 +21,13 @@ int main(void)
 	printf("0\n");
 	}
 	else if ((n % 10) > 5)
-	{
+{
 	printf("greater than 5\n");
-	}
-	else
-	{
+}
+	else	
+{
 	printf("less than 6 and not 0\n");
-	}
+}
 
 	return (0);
 }
