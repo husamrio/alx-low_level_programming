77a3a39fb742fc275ec7f35a776d326209b75353
husamrio <zulfiqrd@gmail.com>
2023-02-17 21:06:59 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+nclude <stdio.h>
+
+/**
+ *  *main - print lowercase a-z
+ *   *Return: Always 0 (Success)
+ *    */
+
+int main(void)
+{
+		char alpha = 'a';
+
+			while (alpha <= 'z')
+					{
+								putchar(alpha);
+										alpha++;
+}											}
+	putchar('\n');
+
+	return (0);
+}
