1208d41328cfa7578061c7acf4ce16ebed3db16b
husamrio <zulfiqrd@gmail.com>
2023-02-21 18:12:52 +0300

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *main - print 0-putchar.c, followed by new line
+ * Return: 0
+ */
+
+int main(void)
+{	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	-putchar('\n');
+
+	return (0);
+}
