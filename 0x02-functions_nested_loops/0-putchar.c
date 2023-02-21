74c6496e5853ddd0b71e5985afff6aeac1c7a589
Hussein Abdullahi <123378862+husamrio@users.noreply.github.com>
2023-02-21 20:17:04 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,18 @@
-hnvkhv kjekj 
+#include "main.h"
+/**
+ * main - main block
+ * Return: 0
+ **/
+int main(void)
+{
+	char x[] = "_putchar";
+
+	int k;
+
+	for (k = 0; k < 8; k++)
+	{
+		_putchar(x[k]);
+	}
+	_putchar('\n');
+	return (0);
+}
