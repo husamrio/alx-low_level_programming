5f3832fc05d753aa81ebf4ae03b1e376d5e1b347
husamrio <zulfiqrd@gmail.com>
2023-03-10 19:30:13 +0300

    libmy.a, main.h

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ * alhamdulilah
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+
+	return (longi);
+}
+
