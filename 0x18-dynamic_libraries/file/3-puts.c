8b95903bee54363a5aade2a9572853e165d877d5
husamrio <zulfiqrd@gmail.com>
2023-06-19 21:55:56 +0300

    Without libraries what have we? We have no past and no future

---
+++ 0x18-dynamic_libraries/file/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string and a new line
+ * @str: pointer to a string to print
+ * husamrio alxhd
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+        int k = 0;
+
+        while (str[k])
+        {
+                _putchar(str[k]);
+                k++;
+        }
+        _putchar('\n');
+}
