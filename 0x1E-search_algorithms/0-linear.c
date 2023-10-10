009a7b63d4e82e9e35d16f377d5aa4be04422f3a
husamrio <zulfiqrd@gmail.com>
2023-10-10 13:19:50 +0300

    Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Linear search algorithm
+ * *********************
+ * @array: integer array
+ * *********************
+ * @size: size of the array
+ * ************************
+ * @value: value to search for
+ * ****************************
+ * Return: Value index in array or -1
+ * *****************************
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!size)
+		goto KILL;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
+	}
+
+KILL:	return (-1);
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+
+/**
+ * *************************************
+ * binary_search - Binary search algorith
+ * *************************************
+ * @array: An array of integers
+ * ****************************
+ * @size: size of the array/list
+ * *****************************
+ * @value: value to find
+ * *****************************
+ * Return: index of the element in the array or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, start, stop;
+
+	if (!size)
+		goto KILL;
+
+	stop = size - 1;
+	i = stop / 2;
+	start = 0;
+
+	for (; start <= stop;)
+	{
+		printf("Searching in array: ");
+		print_array(array, start, stop);
+		if (value == array[i])
+			return (i);
+		else if (value < array[i])
+			stop = i - 1;
+		else
+			start = i + 1;
+		i = (stop - start) / 2;
+		i += start;
+	}
+KILL:	return (-1);
+}
